101d196cdcd6450bbdc9e60a6bc8ceb378baafdf
jgabriel1 <josegabriel@det.ufc.br>
2021-03-08 23:16:01 -0300

    Added toggle on click to each cell and start game button.

--- gol-react/src/components/Cell/index.tsx
+++ gol-react/src/components/Cell/index.tsx
@@ -3,11 +3,14 @@ import React from 'react';
 import { Container, Alive, Dead } from './styles';
 
 interface CellProps {
+  onClick: () => void;
   isAlive?: boolean;
 }
 
-const Cell: React.FC<CellProps> = ({ isAlive }) => {
-  return <Container>{isAlive ? <Alive /> : <Dead />}</Container>;
+const Cell: React.FC<CellProps> = ({ isAlive, onClick }) => {
+  return (
+    <Container onClick={onClick}>{isAlive ? <Alive /> : <Dead />}</Container>
+  );
 };
 
 export default Cell;
--- gol-react/src/components/Grid/index.tsx
+++ gol-react/src/components/Grid/index.tsx
@@ -7,20 +7,24 @@ import Cell from '../Cell';
 import { Container, Row } from './styles';
 
 const Grid: React.FC = () => {
-  const { grid, toggleCell } = useGame();
+  const { grid, toggleCell, startGame } = useGame();
 
   return (
     <Container>
-      {grid.map(row => (
-        <Row>
-          {row.map(isAlive => (
-            <Cell isAlive={isAlive} />
+      {grid.map((row, rowIndex) => (
+        <Row key={String(rowIndex)}>
+          {row.map((isAlive, cellIndex) => (
+            <Cell
+              key={String(`${rowIndex}:${cellIndex}`)}
+              onClick={() => toggleCell(cellIndex, rowIndex)}
+              isAlive={isAlive}
+            />
           ))}
         </Row>
       ))}
 
-      <button type="button" onClick={() => toggleCell(15, 15)}>
-        Click
+      <button type="button" onClick={() => startGame()}>
+        Start
       </button>
     </Container>
   );
