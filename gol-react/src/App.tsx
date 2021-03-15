d30d5a154ebe7179c95787a217a61a84a245f669
jgabriel1 <josegabriel@det.ufc.br>
2021-03-14 22:34:14 -0300

    Added global css reset.

--- gol-react/src/App.tsx
+++ gol-react/src/App.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import Grid from './components/Grid';
 import GameProvider from './hooks/game';
 
+import './global.css';
+
 const App: React.FC = () => {
   return (
     <GameProvider>
