50ed9c8c387fc9dd91c0dca84721ce1d664511e2
jgabriel1 <josegabriel@det.ufc.br>
2021-04-19 23:22:07 -0300

    Added chakra ui provider to the App.

--- gol-react/src/App.tsx
+++ gol-react/src/App.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
 
-import Grid from './components/Grid';
+import { Grid } from './components/Grid';
 import TopBar from './components/TopBar';
 import AppProvider from './hooks';
 
+import { theme } from './styles/theme';
 import './global.css';
 
 const App: React.FC = () => {
   return (
-    <AppProvider>
-      <TopBar />
-      <Grid />
-    </AppProvider>
+    <ChakraProvider theme={theme}>
+      <AppProvider>
+        <TopBar />
+        <Grid />
+      </AppProvider>
+    </ChakraProvider>
   );
 };
 
