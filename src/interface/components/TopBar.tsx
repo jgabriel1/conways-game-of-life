81283b00c3273f0a90e2adbec3586c6cc41fb7b7
jgabriel1 <josegabriel@det.ufc.br>
2021-05-02 22:56:46 -0300

    Fixed TopBar max width.

--- src/interface/components/TopBar.tsx
+++ src/interface/components/TopBar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Flex, Heading, Text } from '@chakra-ui/react';
+import { Box, Button, Flex, Heading, Text } from '@chakra-ui/react';
 
 import { useGame } from '../hooks/game';
 
@@ -14,58 +14,60 @@ export const TopBar: React.FC = () => {
   const title = "Conway's Game of Life";
 
   return (
-    <Flex
-      as="header"
-      w="100%"
-      maxW={1480}
-      h="64px"
-      bg="blue.400"
-      align="center"
-      justify="space-around"
-    >
-      <Heading color="white" size="lg" fontWeight="medium">
-        {title}
-      </Heading>
+    <Box bg="blue.400" px="24px" w="100%">
+      <Flex
+        as="header"
+        w="100%"
+        maxW={1180}
+        h="64px"
+        mx="auto"
+        align="center"
+        justify="space-between"
+      >
+        <Heading color="white" size="lg" fontWeight="medium">
+          {title}
+        </Heading>
 
-      <Flex gridGap="2">
-        <Button
-          type="button"
-          w="100px"
-          onClick={() => toggleGame()}
-          size="lg"
-          colorScheme={gameIsRunning ? 'red' : 'green'}
-        >
-          <Text
-            fontWeight="medium"
-            color={gameIsRunning ? 'gray.800' : 'inherit'}
+        <Flex gridGap="2">
+          <Button
+            type="button"
+            w="100px"
+            onClick={() => toggleGame()}
+            size="lg"
+            colorScheme={gameIsRunning ? 'red' : 'green'}
           >
-            {gameIsRunning ? 'Stop' : 'Start'}
-          </Text>
-        </Button>
+            <Text
+              fontWeight="medium"
+              color={gameIsRunning ? 'gray.800' : 'inherit'}
+            >
+              {gameIsRunning ? 'Stop' : 'Start'}
+            </Text>
+          </Button>
 
-        <Button
-          type="button"
-          w="100px"
-          size="lg"
-          colorScheme="blue"
-          onClick={() => generateRandomGame()}
-          disabled={gameIsRunning}
-        >
-          <Text fontWeight="medium">Generate</Text>
-        </Button>
+          <Button
+            type="button"
+            w="100px"
+            size="lg"
+            colorScheme="blue"
+            onClick={() => generateRandomGame()}
+            disabled={gameIsRunning}
+          >
+            <Text fontWeight="medium">Generate</Text>
+          </Button>
 
-        <Button
-          type="button"
-          w="100px"
-          size="lg"
-          colorScheme="blue"
-          variant="solid"
-          onClick={() => resetGame()}
-          disabled={gameIsRunning}
-        >
-          <Text fontWeight="medium">Clear</Text>
-        </Button>
+          <Button
+            type="button"
+            w="100px"
+            size="lg"
+            colorScheme="blue"
+            variant="solid"
+            onClick={() => resetGame()}
+            disabled={gameIsRunning}
+          >
+            <Text fontWeight="medium">Clear</Text>
+          </Button>
+        </Flex>
       </Flex>
-    </Flex>
+    </Box>
   );
 };
